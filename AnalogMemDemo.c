828b803f74f2b4cbbc22d968a39bfaf107e89ac4
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2019-11-28 00:25:01 +0800

    Analog memory access and write

--- AnalogMemDemo.c
+++ AnalogMemDemo.c
@@ -9,16 +9,23 @@ int32_t c2i(const char *c){
 int main()
 {
 	char a[8]={1,2,3,4,5,6,7,8};
+	//(int32_t)a[0]=0xFFFF0000;  /*ERROR*/
+	//(int32_t)a[4]=0x0000EEEE;
 	printf("%x %x\n",sizeof(a),UCHAR_MAX);
 	for(int i=0;i<8;++i)printf("%x ",a[i]);
 	puts("");
-
+	//Access method 1
 	int32_t b,c;
 	memcpy(&b,&a[0],4);
 	memcpy(&c,&a[4],4);
 	printf("%x %x\n",b,c);
 
 	printf("%x %x\n",c2i(&a[0]),c2i(&a[4]));
-	printf("%x %x\n",C2i(&a[0]),C2i(&a[4])); // ditto
+	//Access mehtod 2
+	printf("%x %x\n",C2i(&a[0]),C2i(&a[4])); 
+	
+	//Write method
+	C2i(&a[0])=0x99887766;
+	printf("%x\n",C2i(&a[0]));
    return 0;
 }
