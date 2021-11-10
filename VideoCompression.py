f8ac0d99b84004e4ca96b32974813d588042f6ae
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2021-11-10 17:23:15 +0800

    Update VideoCompression.py

--- VideoCompression.py
+++ VideoCompression.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 import subprocess
 
-#ffmpeg -i "<source_file>"" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 3 -aq-strength 0.8 -me_method 4 -c:a aac -b:a 320k "<output_file>"
+#ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 3 -aq-strength 0.8 -me_method 4 -c:a aac -b:a 320k "<output_file>"
 #x264 --crf 23 --preset veryslow -r 6 -b 6 -I 600 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.4:0 --aq-mode 3 --aq-strength 0.8 --me tesa -o "<output_file>" "<source_file>"
 
 def convert(outputDir: str, inputPaths: list[str]):
