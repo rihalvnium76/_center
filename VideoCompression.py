71c4bdfc12964849b81fd8a70988990d243e2741
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2021-11-11 11:23:32 +0800

    Update VideoCompression.py

--- VideoCompression.py
+++ VideoCompression.py
@@ -7,13 +7,13 @@ import os
 #ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 3 -aq-strength 0.8 -me_method 4 -c:a copy "<output_file>"
 #x264 --crf 23 --preset veryslow -r 6 -b 6 -I 600 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.4:0 --aq-mode 3 --aq-strength 0.8 --me tesa -o "<output_file>" "<source_file>"
 
-def Convert(outputDir: str, inputPaths: Iterable[str]) -> None:
+def convert(outputDir: str, inputPaths: Iterable[str], allowTrimPath: bool = True) -> None:
     def getCommandLine(inputPath: str, outputPath: str) -> list[str]:
         return ('ffmpeg', '-i', inputPath, '-c:v', 'libx264', '-preset', 'veryslow', '-crf', '23', '-refs', '6', '-bf', '6', '-g', '600', '-keyint_min', '1', '-sc_threshold', '60', '-deblock', '1:1', '-qcomp',
 '0.5', '-psy-rd', '0.4:0', '-aq-mode', '3', '-aq-strength', '0.8', '-me_method', '4', '-c:a', 'copy', outputPath)
 
     def trimPath(path: str) -> str:
-        return path.strip('" ')
+        return path.strip('" ') if allowTrimPath else path
 
     def getOutputPath(inputPath: str) -> str:
         base, ext = os.path.splitext(os.path.basename(inputPath))
@@ -31,9 +31,9 @@ def Convert(outputDir: str, inputPaths: Iterable[str]) -> None:
             print()
             print(e.stderr)
 
-Convert(r'D:\a\1', r'''
+convert(r'D:\a\1', r'''
 D:\_todo\xxxx.mp4
 D:\_todo\yyyy.mp4
 '''.splitlines()) #[1:])
 
-# os.system('pause')
+os.system('pause')
