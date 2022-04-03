940058d80e33e4402fa94649971d67ece81de91d
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-04-03 18:13:16 +0800

    Update VideoCompression.py

--- VideoCompression.py
+++ VideoCompression.py
@@ -3,21 +3,22 @@ from typing import Iterable
 import subprocess
 import os
 
-#ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 3 -aq-strength 0.8 -me_method 4 -c:a aac -b:a 320k "<output_file>"
-#ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 3 -aq-strength 0.8 -me_method 4 -c:a copy "<output_file>"
-#x264 --crf 23 --preset veryslow -r 6 -b 6 -I 600 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.4:0 --aq-mode 3 --aq-strength 0.8 --me tesa -o "<output_file>" "<source_file>"
+# --- use lastest ffmpeg ---
+#ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 2 -aq-strength 0.8 -me_method 2 -c:a copy "<output_file>"
+#ffmpeg -i "<source_file>" -c:v libx264 -preset veryslow -crf 23 -refs 6 -bf 6 -g 600 -keyint_min 1 -sc_threshold 60 -deblock 1:1 -qcomp 0.5 -psy-rd 0.4:0 -aq-mode 2 -aq-strength 0.8 -me_method 2 -c:a copy "<output_file>"
+##x264 --crf 23 --preset veryslow -r 6 -b 6 -I 600 -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.4:0 --aq-mode 2 --aq-strength 0.8 --me umh -o "<output_file>" "<source_file>"
 
 def convert(outputDir: str, inputPaths: Iterable[str], allowTrimPath: bool = True) -> None:
     def getCommandLine(inputPath: str, outputPath: str) -> list[str]:
-        return ('ffmpeg', '-i', inputPath, '-c:v', 'libx264', '-preset', 'veryslow', '-crf', '23', '-refs', '6', '-bf', '6', '-g', '600', '-keyint_min', '1', '-sc_threshold', '60', '-deblock', '1:1', '-qcomp',
-'0.5', '-psy-rd', '0.4:0', '-aq-mode', '3', '-aq-strength', '0.8', '-me_method', '4', '-c:a', 'copy', outputPath)
+        return ('ffmpeg', '-hide_banner', '-i', inputPath, '-c:v', 'libx264', '-preset', 'veryslow', '-crf', '23', '-refs', '6', '-bf', '6', '-g', '600', '-keyint_min', '1', '-sc_threshold', '60', '-deblock', '1:1', '-qcomp',
+'0.5', '-psy-rd', '0.4:0', '-aq-mode', '2', '-aq-strength', '0.8', '-me_method', '2', '-c:a', 'copy', outputPath)
 
     def trimPath(path: str) -> str:
         return path.strip('" ') if allowTrimPath else path
 
     def getOutputPath(inputPath: str) -> str:
         base, ext = os.path.splitext(os.path.basename(inputPath))
-        return os.path.join(outputDir, base + '_x264' + ext)
+        return os.path.join(outputDir, base + '_ffx264' + ext)
     
     outputDir = trimPath(outputDir)
     for i in inputPaths:
@@ -30,6 +31,10 @@ def convert(outputDir: str, inputPaths: Iterable[str], allowTrimPath: bool = Tru
         except subprocess.CalledProcessError as e:
             print()
             print(e.stderr)
+
+convert(r'D:\a\1', [
+    r"D:\_TODO\zzzz.mp4"
+])
 
 convert(r'D:\a\1', r'''
 D:\_todo\xxxx.mp4
