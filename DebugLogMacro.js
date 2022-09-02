539e68aaf41a847298050837753e509fdb2da8cc
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-09-02 23:36:48 +0800

    Update DebugLogMacro.js

--- DebugLogMacro.js
+++ DebugLogMacro.js
@@ -19,24 +19,34 @@
 //   };
 // }
 
-function LiteLogFactory(tag) {
-  const id = String(Math.random()).slice(2);
-  function Log(...v) {
-    console.log(tag, id, ...v);
-  }
-  Log.id = id;
-  Log.tag = tag;
-  return Log;
-}
+// function LiteLogFactory(tag) {
+//   const id = String(Math.random()).slice(2);
+//   function Log(...v) {
+//     console.log(tag, id, ...v);
+//   }
+//   Log.id = id;
+//   Log.tag = tag;
+//   return Log;
+// }
 
-function LiteLogFactoryV2(tag, parent) {
-  const id = String(Math.random()).slice(2);
+// function LiteLogFactoryV2(tag, parent) {
+//   function Log(...v) {
+//     console.log(Log.tag, Log.id, ...v);
+//   }
+//   Log.id = String(Math.random()).slice(2);
+//   Log.tag = tag;
+//   Log.plog = parent ? parent : {};
+//   Log.t = (...v) => Log('<-', Log.plog.tag, Log.plog.id, ...v);
+//   return Log;
+// }
+
+function LiteLogFactoryV3(tag, parent = {}, id = String(Math.random()).slice(2)) {
   function Log(...v) {
     console.log(tag, id, ...v);
   }
-  Log.id = id;
   Log.tag = tag;
-  Log.p = (...v) => {if (parent) Log('<-', parent.tag, parent.id, ...v)};
+  Log.id = id;
+  Log.t = (...v) => Log('<-', parent.tag, parent.id, ...v);
   return Log;
 }
 
@@ -44,10 +54,10 @@ function LiteLogFactoryV2(tag, parent) {
 // function LogFactory(a){let b=String(Math.random()).slice(2),c=console.log;return{tag:a,id:b,n(...d){c(a,b,...d)},g:(...d)=>(...e)=>{console.group(a,b,...d),c(...e),console.groupEnd()}}}
 // function LogFactory(a){let b=String(Math.random()).slice(2),c=console;return{tag:a,id:b,n(...d){c.log(a,b,...d)},g:(...d)=>(...e)=>{c.group(a,b,...d),c.log(...e),c.groupEnd()}}}
 
-function LiteLogFactory(b){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a}
+// function LiteLogFactory(b){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a}
 
-function LiteLogFactoryV2(b,d){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a.p=(...b)=>{d&&a("<-",d.tag,d.id,...b)},a}
-
+// function LiteLogFactoryV2(b,d){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a.p=(...b)=>{d&&a("<-",d.tag,d.id,...b)},a}
+function LiteLogFactoryV3(b,d={},c=String(Math.random()).slice(2)){function a(...a){console.log(b,c,...a)}return a.tag=b,a.id=c,a.t=(...b)=>a("<-",d.tag,d.id,...b),a}
 
 // const log = LogFactory("");
 // log.n();debugger;
