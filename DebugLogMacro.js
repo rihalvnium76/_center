ac8647abbb9edf3d62fa546d74cc423856dcc6d6
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-11-17 01:17:38 +0800

    Update DebugLogMacro.js

--- DebugLogMacro.js
+++ DebugLogMacro.js
@@ -1,45 +1,5 @@
 // https://www.toptal.com/developers/javascript-minifier
 
-// function LogFactory(tag) {
-//   const id = String(Math.random()).slice(2);
-//   const _c = console;
-//   return {
-//     tag,
-//     id,
-//     n(...v) {
-//       _c.log(tag, id, ...v);
-//     },
-//     g(...subtag) {
-//       return (...v) => {
-//         _c.group(tag, id, ...subtag);
-//         _c.log(...v);
-//         _c.groupEnd();
-//       };
-//     }
-//   };
-// }
-
-// function LiteLogFactory(tag) {
-//   const id = String(Math.random()).slice(2);
-//   function Log(...v) {
-//     console.log(tag, id, ...v);
-//   }
-//   Log.id = id;
-//   Log.tag = tag;
-//   return Log;
-// }
-
-// function LiteLogFactoryV2(tag, parent) {
-//   function Log(...v) {
-//     console.log(Log.tag, Log.id, ...v);
-//   }
-//   Log.id = String(Math.random()).slice(2);
-//   Log.tag = tag;
-//   Log.plog = parent ? parent : {};
-//   Log.t = (...v) => Log('<-', Log.plog.tag, Log.plog.id, ...v);
-//   return Log;
-// }
-
 function LiteLogFactoryV3(tag, parent = {}, id = String(Math.random()).slice(2)) {
   function Log(...v) {
     console.log(tag, id, ...v);
@@ -50,17 +10,22 @@ function LiteLogFactoryV3(tag, parent = {}, id = String(Math.random()).slice(2))
   return Log;
 }
 
-// function LogFactory(a){let b=String(Math.random()).slice(2);return{tag:a,id:b,n(...c){console.log(a,b,...c)},g:(...c)=>(...d)=>{console.group(a,b,...c),console.log(...d),console.groupEnd()}}}
-// function LogFactory(a){let b=String(Math.random()).slice(2),c=console.log;return{tag:a,id:b,n(...d){c(a,b,...d)},g:(...d)=>(...e)=>{console.group(a,b,...d),c(...e),console.groupEnd()}}}
-// function LogFactory(a){let b=String(Math.random()).slice(2),c=console;return{tag:a,id:b,n(...d){c.log(a,b,...d)},g:(...d)=>(...e)=>{c.group(a,b,...d),c.log(...e),c.groupEnd()}}}
-
-// function LiteLogFactory(b){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a}
-
-// function LiteLogFactoryV2(b,d){let c=String(Math.random()).slice(2);function a(...a){console.log(b,c,...a)}return a.id=c,a.tag=b,a.p=(...b)=>{d&&a("<-",d.tag,d.id,...b)},a}
 function LiteLogFactoryV3(b,d={},c=String(Math.random()).slice(2)){function a(...a){console.log(b,c,...a)}return a.tag=b,a.id=c,a.t=(...b)=>a("<-",d.tag,d.id,...b),a}
 
-// const log = LogFactory("");
-// log.n();debugger;
+function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), output = console.log, head = n => `[${tag} ${id}]${n && parent ? parent.h(--n) : ""}`, generator = n => (...v) => output(head(n), ...v)) {
+  /** Log */
+  const Log = generator(0)
+  Log.h = head
+  /** Log with parent*/
+  Log.t = generator(1)
+  /** Log with stack */
+  Log.s = generator(-1)
+  return Log
+}
 
-// const log2 = (a=>{let b=String(Math.random()).slice(2);return{tag:a,id:b,n(...c){console.log(a,b,...c)},g:(...c)=>(...d)=>{console.group(a,b,...c),console.log(...d),console.groupEnd()}}})("");
-// log.n();debugger;
+function LogFactoryV4(o,t,n=String(Math.random()).slice(2),r=console.log,c=r=>`[${o} ${n}]${r&&t?t.h(--r):""}`,e=o=>(...t)=>r(c(o),...t)){let l=e(0);return l.h=c,l.t=e(1),l.s=e(-1),l}
+
+let a=LogFactoryV4('A'),b=LogFactoryV4('B',a),c=LogFactoryV4('C',b)
+c(1)
+c.t(1)
+c.s(1)
