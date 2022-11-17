8a24e58dc655260b564e20a5d231f2adbd764221
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-11-17 12:56:17 +0800

    Update DebugLogMacro.js

--- DebugLogMacro.js
+++ DebugLogMacro.js
@@ -12,7 +12,7 @@ function LiteLogFactoryV3(tag, parent = {}, id = String(Math.random()).slice(2))
 
 function LiteLogFactoryV3(b,d={},c=String(Math.random()).slice(2)){function a(...a){console.log(b,c,...a)}return a.tag=b,a.id=c,a.t=(...b)=>a("<-",d.tag,d.id,...b),a}
 
-function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), output = console.log, head = n => `[${tag} ${id}]${n && parent ? parent.h(--n) : ""}`, generator = n => (...v) => output(head(n), ...v)) {
+function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), head = n => `[${tag} ${id}]${n && parent ? parent.h(--n) : ""}`, generator = n => (...v) => console.log(head(n), ...v)) {
   /** Log */
   const Log = generator(0)
   Log.h = head
@@ -23,10 +23,12 @@ function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), output =
   return Log
 }
 
-function LogFactoryV4(o,t,n=String(Math.random()).slice(2),r=console.log,c=r=>`[${o} ${n}]${r&&t?t.h(--r):""}`,e=o=>(...t)=>r(c(o),...t)){let l=e(0);return l.h=c,l.t=e(1),l.s=e(-1),l}
-/// function LogFactoryV4_TS(o:string,t:any,n=String(Math.random()).slice(2),r=console.log,c=(r:number)=>`[${o} ${n}]${r&&t?t.h(--r):""}`,e=(o:number)=>(...t:any)=>r(c(o),...t)){let l:any=e(0);return l.h=c,l.t=e(1),l.s=e(-1),l}function LogFactoryV4_TS(o:string,t:any,n=String(Math.random()).slice(2),r=console.log,c=(r:number)=>`[${o} ${n}]${r&&t?t.h(--r):""}`,e=(o:number)=>(...t:any)=>r(c(o),...t)){let l:any=e(0);return l.h=c,l.t=e(1),l.s=e(-1),l}
+function LogFactoryV4(o,t,n=String(Math.random()).slice(2),r=r=>`[${o} ${n}]${r&&t?t.h(--r):""}`,c=o=>(...t)=>console.log(r(o),...t)){let e=c(0);return e.h=r,e.t=c(1),e.s=c(-1),e}
+
+/// function LogFactoryV4(o:string,t:any,n=String(Math.random()).slice(2),r=(r:any)=>`[${o} ${n}]${r&&t?t.h(--r):""}`,c=(o:any)=>(...t:any)=>console.log(r(o),...t)){let e:any=c(0);return e.h=r,e.t=c(1),e.s=c(-1),e}
+
 
 let a=LogFactoryV4('A'),b=LogFactoryV4('B',a),c=LogFactoryV4('C',b)
 c(1)
-c.t(1)
-c.s(1)
+c.t(2)
+c.s(4)
