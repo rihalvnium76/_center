6fe4fc214068b1d24dfb0c73501feee34e7ee80b
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-11-17 19:59:55 +0800

    Update DebugLogMacro.js

--- DebugLogMacro.js
+++ DebugLogMacro.js
@@ -26,7 +26,7 @@ function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), head = n
 function LogFactoryV4(o,t,n=String(Math.random()).slice(2),r=r=>`[${o} ${n}]${r&&t?t.h(--r):""}`,c=o=>(...t)=>console.log(r(o),...t)){let e=c(0);return e.h=r,e.t=c(1),e.s=c(-1),e}
 
 /// function LogFactoryV4(o:string,t:any,n=String(Math.random()).slice(2),r=(r:any)=>`[${o} ${n}]${r&&t?t.h(--r):""}`,c=(o:any)=>(...t:any)=>console.log(r(o),...t)){let e:any=c(0);return e.h=r,e.t=c(1),e.s=c(-1),e}
-
+/// interface Log{(...v:any):void;h(n:number):string;t(...v:any):void;s(...v:any):void};function LogFactoryV4(o:string,t?:Log):Log;function LogFactoryV4(o:string,t?:Log,n=String(Math.random()).slice(2),r=(r:number)=>`[${o} ${n}]${r&&t?t.h(--r):""}`,c=(o:number)=>(...t:any)=>console.log(r(o),...t)):Log{let e:any=c(0);return e.h=r,e.t=c(1),e.s=c(-1),e}
 
 let a=LogFactoryV4('A'),b=LogFactoryV4('B',a),c=LogFactoryV4('C',b)
 c(1)
