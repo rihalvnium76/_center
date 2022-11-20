2c332608f147d6e2ba899b139896463491df61c6
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2022-11-21 01:27:51 +0800

    Update DebugLogMacro.js

--- DebugLogMacro.js
+++ DebugLogMacro.js
@@ -13,8 +13,8 @@ function LiteLogFactoryV3(tag, parent = {}, id = String(Math.random()).slice(2))
 /** @deprecated */
 function LiteLogFactoryV3(b,d={},c=String(Math.random()).slice(2)){function a(...a){console.log(b,c,...a)}return a.tag=b,a.id=c,a.t=(...b)=>a("<-",d.tag,d.id,...b),a}
 
-// V4.3
-function LogFactoryV4(tag, parent, head = n => `[${tag} ${String(Math.random()).slice(2)}]${n && parent ? parent.h(--n) : ""}`, generator = n => (...v) => console.log(head(n), ...v), Log = generator(0)) {
+// v4.4
+function LogFactoryV4(tag, parent, id = String(Math.random()).slice(2), head = n => `[${tag} ${id}]${n && parent ? parent.h(--n) : ""}`, generator = n => (...v) => console.log(head(n), ...v), Log = generator(0)) {
   Log.h = head
   /** Log with parent*/
   Log.t = generator(1)
@@ -23,15 +23,12 @@ function LogFactoryV4(tag, parent, head = n => `[${tag} ${String(Math.random()).
   return Log
 }
 
-function LogFactoryV4(o,n,r=r=>`[${o} ${String(Math.random()).slice(2)}]${r&&n?n.h(--r):""}`,t=o=>(...n)=>console.log(r(o),...n),c=t(0)){return c.h=r,c.t=t(1),c.s=t(-1),c}
+function LogFactoryV4(o,n,r=String(Math.random()).slice(2),t=t=>`[${o} ${r}]${t&&n?n.h(--t):""}`,c=o=>(...n)=>console.log(t(o),...n),a=c(0)){return a.h=t,a.t=c(1),a.s=c(-1),a}
 
 /// with comments
-function LogFactoryV4(/** string tag */ o, /** parent Log */ n, /** @immutable log head (tag + id) */ r=r=>`[${o} ${String(Math.random()).slice(2)}]${r&&n?n.h(--r):""}`, /** @immutable log function generator */ t=o=>(...n)=>console.log(r(o),...n), /** @immutable Logger (usage: log(...)) */ c=t(0)){return c.h=r, /** log with parent head (usage: log.t(...)) */ c.t=t(1), /** log with stack (usage: log.s(...)) */ c.s=t(-1), c}
+function LogFactoryV4(/** string tag */ o, /** parent Log */ n,/** @immutable id */ r=String(Math.random()).slice(2), /** @immutable log head (tag + id) */ t=t=>`[${o} ${r}]${t&&n?n.h(--t):""}`, /** @immutable log function generator */ c=o=>(...n)=>console.log(t(o),...n), /** @immutable Logger (usage: log(...)) */ a=c(0)){return a.h=t, /** log with parent head (usage: log.t(...)) */ a.t=c(1), /** log with stack (usage: log.s(...)) */ a.s=c(-1),a}
 
-/// v4.2 TS
-// interface Log {(...v: any): void; h(n: number): string; t(...v: any): void; s(...v: any): void}
-// function LogFactoryV4T(tag: string, parentLog?: Log): Log;
-// function LogFactoryV4T(o:string,n:any,r=(r:any)=>`[${o} ${String(Math.random()).slice(2)}]${r&&n?n.h(--r):""}`,t=(o:any)=>(...n:any)=>console.log(r(o),...n),c:any=t(0)){return c.h=r,c.t=t(1),c.s=t(-1),c}
+// function LogFactoryV4(o:string,n:any,r=String(Math.random()).slice(2),t=(t:any)=>`[${o} ${r}]${t&&n?n.h(--t):""}`,c=(o:any)=>(...n:any)=>console.log(t(o),...n),a:any=c(0)){return a.h=t,a.t=c(1),a.s=c(-1),a}
 
 let a=LogFactoryV4('A'),b=LogFactoryV4('B',a),c=LogFactoryV4('C',b)
 c(1)
