25fc88b406cb0ad344175dce8cdb10a0c266eabb
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2024-09-18 19:59:13 +0800

    Update SlotString.java

--- SlotString.java
+++ SlotString.java
@@ -1,103 +1,140 @@
-package util;
-
 import java.math.BigDecimal;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 public class SlotString {
-  /**
-   * 将 src 中的占位符（${var}、#{var}、{var}）替换为 dest 中键为 var 的值.<br>
-   * dest 中的值默认使用 toString() 转为字符串，BigDecimal 类型的值会使用 toPlainString() 转换，null 值会被转为 "".<br>
-   * 支持使用 \ 转义符号为普通文字.<br>
-   * 使用不支持的语法会导致非预期的解析.
-   * 
-   * @param src  源字符串.
-   * @param dest 替换表.
-   * @return 输出字符串.
-   */
-  public static String format(String src, Map<String, Object> dest) {
-    if (src == null || src.isEmpty()) {
-      return src;
+  private static final Integer TEXT_TYPE = 0;
+  private static final Integer KEY_TYPE = 1;
+  
+  private final StringBuilder res = new StringBuilder();
+  private final StringBuilder key = new StringBuilder();
+  private final ArrayList<String> texts;
+  private final ArrayList<Integer> types;
+  
+  public SlotString() {
+    texts = null;
+    types = null;
+  }
+  public SlotString(String pattern) {
+    if (pattern == null) {
+      texts = null;
+      types = null;
+      return;
     }
-    StringBuilder res = new StringBuilder();
-    StringBuilder key = new StringBuilder();
+    if (pattern.isEmpty()) {
+      texts = new ArrayList<>(Collections.singletonList(""));
+      types = new ArrayList<>(Collections.singletonList(TEXT_TYPE));
+      return;
+    }
+    texts = new ArrayList<>();
+    types = new ArrayList<>();
+    compile(pattern);
+  }
+  
+  public String qformat(String pattern, Map<String, Object> vars) {
+    if (pattern == null || pattern.isEmpty()) {
+      return pattern;
+    }
+    if (vars == null) {
+      parse(pattern, Collections.emptyMap());
+    } else {
+      parse(pattern, vars);
+    }
+    String ret = res.toString();
+    res.setLength(0);
+    key.setLength(0);
+    return ret;
+  }
+  
+  private void compile(String pattern) {
+    parse(pattern, null);
+    if (res.length() != 0) {
+      texts.add(res.toString());
+      types.add(TEXT_TYPE);
+    }
+    res.setLength(0);
+    key.setLength(0);
+  }
+  
+  public String format(Map<String, Object> vars) {
+    if (types == null) {
+      return null;
+    }
+    for (int i = 0; i < types.size(); ++i) {
+      Integer type = types.get(i);
+      String text = texts.get(i);
+      if (type == TEXT_TYPE) {
+        res.append(text);
+      } else if (type == KEY_TYPE) {
+        String val = asString(text, vars);
+        if (val != null && !val.isEmpty()) {
+          res.append(val);
+        }
+      }
+    }
+    String ret = res.toString();
+    res.setLength(0);
+    return ret;
+  }
+  
+  private void parse(String pattern, Map<String, Object> vars) {
     int state = 0;
-    char prev = 0;
-    for (int i = 0; i < src.length(); ++i) {
-      char c = src.charAt(i);
+    for (int i = 0; i < pattern.length(); ++i) {
+      char c = pattern.charAt(i);
       if (state == 0) {
-        if (c == '#' || c == '$') {
+        if (c == '{') {
           state = 1;
-          prev = c;
-        } else if (c == '{') {
-          state = 2;
-          key.setLength(0);
         } else if (c == '\\') {
-          state = 3;
-        } else  {
+          state = 2;
+        } else {
           res.append(c);
         }
       } else if (state == 1) {
-        if (c == '{') {
-          state = 2;
-          key.setLength(0);
-        } else {
-          state = 0;
-          res.append(prev);
-          --i; // re-parse
-        }
-      } else if (state == 2) {
         if (c == '}') {
           state = 0;
-          Object val = null;
-          if (dest != null) {
-            val = dest.get(key.toString());
+          if (vars == null) {
+            parseCompile();
+          } else {
+            parseQformat(vars);
           }
-          if (val == null) {
-            val = "";
-          } else if (val instanceof BigDecimal) {
-            val = ((BigDecimal) val).toPlainString();
-          }
-          res.append(val);
-        } else if (c == '\\') {
-          state = 4;
         } else {
           key.append(c);
         }
-      } else if (state == 3) {
+      } else if (state == 2) {
         state = 0;
         res.append(c);
-      } else if (state == 4) {
-        state = 2;
-        key.append(c);
       }
     }
-    return res.toString();
   }
-
-  public static void main(String[] args) {
-    long t = System.currentTimeMillis();
-    Map<String, Object> dest = new HashMap<>();
-    dest.put("123", -123);
-    dest.put("ABC", "-ABC");
-    dest.put("123_ABC", "-123_ABC");
-    System.out.println(format(" [${123}] [#{ABC}] [{123_ABC}]", dest));
-    System.out.println(format(" [${}] [#{void}] [{-1}]", dest));
-    dest.put("0", new BigDecimal("1E+10"));
-    dest.put("1", 1.0);
-    dest.put("2", 1.0f);
-    dest.put("3", 1000000);
-    dest.put("4", 1000000L);
-    dest.put("\\", 999);
-    dest.put("", 888);
-    dest.put("}", 777);
-    System.out.println(format(" [${0}] [#{1}] [{2}] <${3}> (#{4})", dest));
-    System.out.println(format(" [\\#\\{0}] [\\$\\{0}] [\\{0}]  \\{{0}\\}", dest));
-    System.out.println(System.currentTimeMillis() - t);
-
-    System.out.println(format(" [\\?{0}] \\{{0}}", dest));
-    System.out.println(format(" [#${0}] [#\\${0}] [#$\\{0}]", dest));
-    System.out.println(format(" [{0]", dest));
-    System.out.println(format(" <{\\\\}}> [{}] [{\\}}] ", dest));
+  
+  private void parseQformat(Map<String, Object> vars) {
+    String val = asString(key.toString(), vars);
+    if (val != null && !val.isEmpty()) {
+      res.append(val);
+    }
+    key.setLength(0);
+  }
+  
+  private void parseCompile() {
+    if (res.length() != 0) {
+      texts.add(res.toString());
+      types.add(TEXT_TYPE);
+      res.setLength(0);
+    }
+    texts.add(key.toString());
+    types.add(KEY_TYPE);
+    key.setLength(0);
+  }
+  
+  protected String asString(String key, Map<String, Object> vars) {
+    Object val = vars.get(key);
+    if (val == null) {
+      return "";
+    }
+    if (val instanceof BigDecimal) {
+      return ((BigDecimal) val).toPlainString();
+    }
+    return val.toString();
   }
 }
