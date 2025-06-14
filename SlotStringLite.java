0857af53c27fab686db5d65245afa1212d1768cd
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2025-06-14 12:13:20 +0800

    Update SlotStringLite.java

--- SlotStringLite.java
+++ SlotStringLite.java
@@ -6,42 +6,35 @@ import java.util.regex.Pattern;
 
 public class SlotStringLite {
 
-  private static final Pattern SLOT = Pattern.compile("(?<!\\{)\\{[^{}\r\n\t]*}(?!})|\\{\\{|}}");
-  
-  public static String format(String template, Map<String, Object> values) {
+  private static final Pattern SLOT_PATTERN = Pattern.compile("\\{(.+?)}");
+
+  public static String format(String template, Map<String, Object> values, StringBuffer buffer) {
     if (template == null || template.isEmpty()) {
       return "";
     }
     if (values == null) {
       values = Collections.emptyMap();
     }
-    
-    Matcher matcher = SLOT.matcher(template);
-    StringBuilder sb = new StringBuilder(template.length());
+    StringBuffer buffer0 = buffer;
+    if (buffer0 == null) {
+      buffer0 = new StringBuffer((int)(template.length() * 1.5F));
+    }
+    Matcher matcher = SLOT_PATTERN.matcher(template);
     while (matcher.find()) {
-      String slot = matcher.group();
-      if (slot.length() == 2 && slot.charAt(0) == slot.charAt(1)) {
-        // Escape
-        matcher.appendReplacement(sb, String.valueOf(slot.charAt(0)));
-        continue;
-      }
-      String key = slot.substring(1, slot.length() - 1);
+      String key = matcher.group(1);
       Object value = values.get(key);
       if (value == null) {
-        matcher.appendReplacement(sb, "");
+        value = "";
       } else if (value instanceof BigDecimal) {
-        matcher.appendReplacement(sb, ((BigDecimal) value).toPlainString());
-      } else {
-        matcher.appendReplacement(sb, value.toString());
+        value = ((BigDecimal) value).toPlainString();
       }
+      matcher.appendReplacement(buffer0, value.toString());
     }
-    matcher.appendTail(sb);
-    
-    return sb.toString();
-  }
-  
-  public static void main(String[] args) {
-    System.out.println(format("0 {A} {B} {C} {{D}} {{{{E}}}} {{{{{{F}}}}}} {} 9", Map.of("A", "1", "B", 2, "C", new BigDecimal("1.2E+3"))));
-    System.out.println(format("{A}{{B}}{}", Collections.singletonMap("A", "{{1}}")));
+    matcher.appendTail(buffer0);
+    String ret = buffer0.toString();
+    if (buffer != null) {
+      buffer.setLength(0);
+    }
+    return ret;
   }
 }
