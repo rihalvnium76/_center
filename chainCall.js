b4fb9d7a23fd1d3c2529914868215b33bd5887b4
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2025-06-14 11:30:53 +0800

    Update chainCall.js

--- chainCall.js
+++ chainCall.js
@@ -1,23 +1,26 @@
-// Optional chaining polyfill
+// Optional chaining polyfill and enhancement
 var chainCall = (function (fieldCache) {
-  return function (obj, path, nullValue, computed, separator, bound) {
+  var getNullDefault = function (nullDefault, computed) {
+    if (computed && typeof nullDefault === "function") {
+      return nullDefault();
+    }
+    return nullDefault;
+  };
+  return function (obj, path, nullDefault, computed, separator, bound) {
     if (obj == null) {
-      if (typeof nullValue === "function" && computed) {
-        return nullValue();
-      }
-      return nullValue;
+      return getNullDefault(nullDefault, computed);
+    }
+    if (path == null) {
+      return obj;
     }
     var fields = path;
     if (!Array.isArray(path)) {
-      if (path == null) {
-        return obj;
-      }
       path = String(path);
       separator = separator || ".";
       var key = String(separator.length) + separator + path;
       fields = fieldCache[key];
       if (!fields) {
-        fields = fieldCache[key] = path.split(separator);
+        fieldCache[key] = fields = path.split(separator);
       }
     }
     var v1, v2, v3 = obj;
@@ -26,22 +29,18 @@ var chainCall = (function (fieldCache) {
       v2 = v3;
       v3 = fields[i];
       if (Array.isArray(v3)) {
-        if (typeof v2 === "function") {
-          v3 = v2.apply(v1, v3);
-        } else {
-          v3 = null;
+        if (typeof v2 !== "function") {
+          return getNullDefault(nullDefault, computed);
         }
+        v3 = v2.apply(v1, v3);
       } else {
         v3 = v2[v3];
       }
       if (v3 == null) {
-        if (typeof nullValue === "function" && computed) {
-          return nullValue();
-        }
-        return nullValue;
+        return getNullDefault(nullDefault, computed);
       }
     }
-    if (typeof v3 === "function" && bound) {
+    if (bound && typeof v3 === "function") {
       return v3.bind(v2);
     }
     return v3;
