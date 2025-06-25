80cb318e1f8e2cb952d345aa6517521a29e0f6ed
pmwindcl <57667144+rihalvnium76@users.noreply.github.com>
2025-06-25 17:31:57 +0800

    Update hideLastGitCommitDate.bat

--- hideLastGitCommitDate.bat
+++ hideLastGitCommitDate.bat
@@ -1,6 +1,13 @@
 @echo off
-set "REPO_PATH=%~d0\repo"
-set "GIT_AUTHOR_DATE=1970-01-01T08:00:00"
+set "PREV_PATH=%~dp0"
+set "REPO_PATH=%~d0\page"
+set "GIT_AUTHOR_DATE=1970-01-01T08:00:00 +0000"
 set "GIT_COMMITTER_DATE=%GIT_AUTHOR_DATE%"
 
-git --git-dir="%REPO_PATH%\.git" --work-tree="%REPO_PATH%" commit --amend --no-edit --date="%GIT_AUTHOR_DATE%"
+cd /d %REPO_PATH%
+git commit --amend --no-edit --date="%GIT_AUTHOR_DATE%"
+echo.
+git log -1 --pretty=fuller
+cd /d %PREV_PATH%
+
+timeout /t 5 >nul
